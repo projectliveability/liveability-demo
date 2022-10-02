0c20a0ffb4b542c19f06ef8bd40b91e5a9d4b312
projectliveability <114167710+projectliveability@users.noreply.github.com>
2022-10-02 20:24:06 +1100

    Update rollback_setup.sh

--- rollback_setup.sh
+++ rollback_setup.sh
@@ -11,7 +11,6 @@ export MYSQL_PORT="3306"
 export MYSQL_USER="datastream"
 export MYSQL_PASS="12345678"
 
-
 #Parameters for the Datastream Connection Profile generation.
 export MYSQL_CONN_PROFILE="liveability-mysql-conprofile"
 export GCS_CONN_PROFILE="liveability-gcs-conprofile"
@@ -26,8 +25,6 @@ export DS_MYSQL_GCS_NAME="liveability-mysql-to-gcs-stream"
 export DS_SOURCE_JSON="mysql_source_user_activities_config.json"
 export DS_TARGET_JSON="gcs_destination_user_activities_config.json"
 
-
-
 export BQ_DATASET="liveability"
 
 # Cancelling Dataflow job
@@ -38,15 +35,22 @@ gcloud datastream streams delete ${DS_MYSQL_GCS_NAME} --location=${LOCATION}
 gcloud datastream connection-profiles delete ${GCS_CONN_PROFILE} --location=${LOCATION}
 gcloud datastream connection-profiles delete ${MYSQL_CONN_PROFILE} --location=${LOCATION}
 
-
 # Removes the pubsub topic and subsctiption.
 gcloud pubsub subscriptions delete ${DS_PUBSUB_SUBSCRIPTION}
 gcloud pubsub topics delete ${DS_PUBSUB_TOPIC}
 
+# Removes the pubsub notification on GCS bucket.
+gsutil notification delete gs://${PROJECT_ID}
 
+# Delete the cloud sql instance
+gcloud sql instances delete ${MYSQL_INSTANCE}
+
+#Clean up the GCS bucket and its contents.
+gsutil rm -r "gs://${PROJECT_ID}"
+gsutil rb -f "gs://${PROJECT_ID}"
 
 #Delete BigQuery dataset
-bq rm -r -f "liveability
+bq rm -r -f ${PROJECT_ID}:${BQ_DATASET}
 
 # To remove the service accounts we have.
 gcloud iam service-accounts delete ${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com
@@ -64,24 +68,6 @@ gcloud services disable appengine.googleapis.com --force
 gcloud services disable sqladmin.googleapis.com --force 
 #datastream
 gcloud services disable datastream.googleapis.com --force 
-#pubsubapi
-gcloud services disable pubsub.googleapis.com --force 
-#cloudbuild
-gcloud services disable cloudbuild.googleapis.com --force 
-#gcloud services disable compute.googleapis.com --force 
-#gcloud services disable iam.googleapis.com --force 
-#cloud composer
-gcloud services disable composer.googleapis.com --force 
-
-
-#Clean up the GCS bucket and its contents.
-gcloud storage rm --recursive "gs://${PROJECT_ID}"
-
-
-# Delete the cloud sql instance
-
-gcloud sql instances delete ${MYSQL_INSTANCE}
-
 
 
 
